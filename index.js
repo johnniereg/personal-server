e35b96f0a798c55b5b7f6c27c33fae470d8f65d2
johnniereg <johnniereg@gmail.com>
2018-11-24 11:27:16 -0800

    Add more Last FM api call routes

--- index.js
+++ index.js
@@ -12,25 +12,14 @@ const lastfm = {
 };
 
 app.get('/', (req, res) => {
-  res.send("(☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)");
+  res.send(`ಠ‿↼`);
 })
 
-app.get('/api/lastfm/recent-tracks', (req, res) => {
-  let targetUrl =
-    `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json&extended=1&limit=200&from=1514764800`;
-
-  request(targetUrl, (error, response) => {
-    if (error) {
-      console.log(error);
-    } else {
-      res.send(response.body);
-    }
-  });
-});
-
+// User details from Last FM
 app.get('/api/lastfm/user-info', (req, res) => {
+  let method = 'user.getinfo';
   let targetUrl = 
-    `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json`;
+  `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json`;
 
   request(targetUrl, (error, response) => {
     if (error) {
@@ -40,6 +29,108 @@ app.get('/api/lastfm/user-info', (req, res) => {
     }
   });
 });
+
+// Last 200 played tracks since app built
+app.get('/api/lastfm/recent-tracks', (req, res) => {
+  let method = 'user.getrecenttracks';
+  let targetUrl =
+    `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json&extended=1&limit=200&from=1514764800`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
+// Top artists over the last 12 months
+app.get('/api/lastfm/top-artists', (req, res) => {
+  let method = 'user.gettopartists';
+  let targetUrl = `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json&period=12month`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
+// Top 50 albums over last 12 months
+app.get('/api/lastfm/top-albums', (req, res) => {
+  let method = 'user.gettopalbums';
+  let targetUrl = `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json&period=12month`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
+// Top 50 tracks over last 12 months
+app.get('/api/lastfm/top-tracks', (req, res) => {
+  let method = 'user.gettoptracks';
+  let targetUrl = `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json&period=12month`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
+// Top artists over last week
+app.get('/api/lastfm/weekly-artist-chart', (req, res) => {
+  let method = 'user.getweeklyartistchart';
+  let targetUrl = `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
+// Top albums over last week
+app.get('/api/lastfm/weekly-album-chart', (req, res) => {
+  let method = 'user.getweeklyalbumchart';
+  let targetUrl = `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
+// Top tracks over last week
+app.get("/api/lastfm/weekly-track-chart", (req, res) => {
+  let method = "user.getweeklytrackchart";
+  let targetUrl = `http://ws.audioscrobbler.com/2.0/?method=${method}&user=${
+    lastfm.user
+  }&api_key=${lastfm.apiKey}&format=json`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server heart is beating, listen at ${PORT}`);
