520c5882dc8d803dcdecb56891f88bd4f0738859
johnniereg <johnniereg@gmail.com>
2018-11-24 11:05:36 -0800

    Add user info route, made routes more specific

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const request = require('request');
-const env = require('dotenv').config();
 
-const test = process.env.TEST;
+require('dotenv').config();
 
 const PORT = process.env.PORT || 8080
 const app = express();
@@ -13,10 +12,10 @@ const lastfm = {
 };
 
 app.get('/', (req, res) => {
-  // Scraping to come
-  console.log("Thump, thump, thump...");
-  console.log(test);
+  res.send("(☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)");
+})
 
+app.get('/api/lastfm/recent-tracks', (req, res) => {
   let targetUrl =
     `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json&extended=1&limit=200&from=1514764800`;
 
@@ -27,7 +26,19 @@ app.get('/', (req, res) => {
       res.send(response.body);
     }
   });
+});
 
+app.get('/api/lastfm/user-info', (req, res) => {
+  let targetUrl = 
+    `http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${lastfm.user}&api_key=${lastfm.apiKey}&format=json`;
+
+  request(targetUrl, (error, response) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(response.body);
+    }
+  });
 });
 
 app.listen(PORT, () => {
